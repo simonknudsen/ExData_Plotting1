dcd5ca6ea632bbaaa3831dd2b2b6a25d8b9084a5
Simon Knudsen <simonjknudsen@gmail.com>
2015-07-12 17:42:25 +1200

    Added reading data bit

--- plot4.R
+++ plot4.R
@@ -1,3 +1,19 @@
+hhpower_all <- read.delim("household_power_consumption.txt", sep = ";", stringsAsFactors=F)
+
+is.na(hhpower_all) <- hhpower_all=="?"
+
+hhpower_all$Time <- strptime(paste(hhpower_all$Date, " ", hhpower_all$Time), format = "%d/%m/%Y %H:%M:%S")
+hhpower_all$Date <- as.Date(hhpower_all$Date, "%d/%m/%Y")
+hhpower_all$Global_active_power <- as.numeric(hhpower_all$Global_active_power)
+hhpower_all$Global_reactive_power <- as.numeric(hhpower_all$Global_reactive_power)
+hhpower_all$Global_intensity <- as.numeric(hhpower_all$Global_intensity)
+hhpower_all$Voltage <- as.numeric(hhpower_all$Voltage)
+hhpower_all$Sub_metering_1 <- as.numeric(hhpower_all$Sub_metering_1)
+hhpower_all$Sub_metering_2 <- as.numeric(hhpower_all$Sub_metering_2)
+hhpower_all$Sub_metering_3 <- as.numeric(hhpower_all$Sub_metering_3)
+
+hhpower <- subset(hhpower_all , Date == as.Date("2007-02-01") | Date == as.Date("2007-02-02"))
+
 # --- Plot 4
 par(mfrow = c(2, 2), mar = c(4, 4, 2, 1), oma = c(0, 0, 2, 0), 
     cex.lab=0.6, cex.axis=0.6, cex.main=0.6, cex.sub=0.6) 
